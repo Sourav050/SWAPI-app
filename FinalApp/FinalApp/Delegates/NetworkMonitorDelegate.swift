e447e8fe078a101b6c7e6c8b1324778e68d4d874
Sourav050 <102278956+Sourav050@users.noreply.github.com>
2024-02-14 17:52:11 +0530

    Update NetworkMonitorDelegate.swift

--- FinalApp/FinalApp/Delegates/NetworkMonitorDelegate.swift
+++ FinalApp/FinalApp/Delegates/NetworkMonitorDelegate.swift
@@ -1,12 +1,1 @@
-//
-//  NetworkMonitorDelegate.swift
-//  FinalApp
-//
-//  Created by Sourav Choubey on 12/02/24.
-//
 
-import Foundation
-
-protocol NetworkMonitorDelegate: AnyObject {
-    func networkStatusChanged(isConnected: Bool)
-}
